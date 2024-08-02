0bab7c51e19282e1c0ec1f0e5082c19756548132
sultanBytes <sultankhand11@gmail.com>
2024-08-02 15:40:34 +0530

    solved

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,12 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import { userSlice } from '../SliceStore/Slice'
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../SliceStore/Slice'; // Correctly import the default export
 
 export const Store = configureStore({
-        reducer:{
-            user:userSlice
-        }
-    })
-  
-
-
+    reducer: {
+        user: userReducer
+    }
+});
